834ad5b1ad5b4e4cd6d1ce74cf6cbf9fc6fb60aa
markintoshplus <deveramarkcedrick10@gmail.com>
2025-02-27 21:20:22 +0800

    New activity: JS Challenge

---
+++ 5_JavaScript-Fundamentals/1_JS-Challenge/math_operations.js
@@ -0,0 +1,34 @@
+// Task 1
+let item1 = 10;
+let item2 = 20;
+let item3 = 30;
+let totalCost = item1 + item2 + item3;
+console.log("The total cost of the items is: $" + totalCost);
+
+// Task 2
+let num1 = 2;
+let num2 = 3;
+let num3 = 10;
+let average = (num1 + num2 + num3) / 3;
+console.log("The average is: " + average);
+
+// Task 3
+let number = 5;
+if (number % 2 == 0) {
+    console.log(number + " is an even number.");
+} else {
+    console.log(number + " is an odd number.")
+}
+
+// Task 4
+let origPrice = 100;
+let discountPercent = 20;
+let discountAmount = (origPrice * discountPercent) / 100;
+let discountPrice = origPrice - discountAmount;
+console.log("The discounted price is: $" + discountPrice);
+
+// Bonus Task
+let finalPrice = 80;
+let discountPercentage = 20;
+let originalPrice = finalPrice / (1 - discountPercentage / 100);
+console.log("The original price before the discount was: $" + originalPrice);
