5499fa80b34f6f489a19efb1027fc80168f98bd3
markintoshplus <deveramarkcedrick10@gmail.com>
2025-03-08 00:23:41 +0800

    Fixed typos

---
+++ 7_JavaScript-DOM-Manipulation/5_Bank-Account-Manager/script.js
@@ -0,0 +1,10 @@
+// Initial balance
+let currentBalance = 1000;
+
+// Select DOM elements
+const balanceDisplay = document.getElementById("balance");
+const transactionAmountInput = document.getElementById("transaction-amount");
+const depositBtn = document.getElementById("deposit-btn");
+const withdrawBtn = document.getElementById("withdraw-btn");
+const message = document.getElementById("message");
+
